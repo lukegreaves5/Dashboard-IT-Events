96a40ffcf04d684db6e6027e636d387d511321f8
Luke Greaves <lgreaves01@manhattan.edu>
2019-10-31 11:31:59 -0400

    Update to styles

--- index.js
+++ index.js
@@ -1345,81 +1345,6 @@ const data = [
     }
   ];
 
-  console.log(data[0]);
-
-
-
-
-
-
-
-$(function () {
-
-    'use strict';
-  
-    (function () {
-  
-      var aside = $('.side-nav'),
-  
-          showAsideBtn = $('.show-side-btn'),
-  
-          contents = $('#contents');
-  
-      showAsideBtn.on("click", function () {
-  
-        $("#" + $(this).data('show')).toggleClass('show-side-nav');
-  
-        contents.toggleClass('margin');
-  
-      });
-  
-      if ($(window).width() <= 767) {
-  
-        aside.addClass('show-side-nav');
-  
-      }
-      $(window).on('resize', function () {
-  
-        if ($(window).width() > 767) {
-  
-          aside.removeClass('show-side-nav');
-  
-        }
-  
-      });
-  
-      // dropdown menu in the side nav
-      var slideNavDropdown = $('.side-nav-dropdown');
-  
-      $('.side-nav .categories li').on('click', function () {
-  
-        $(this).toggleClass('opend').siblings().removeClass('opend');
-  
-        if ($(this).hasClass('opend')) {
-  
-          $(this).find('.side-nav-dropdown').slideToggle('fast');
-  
-          $(this).siblings().find('.side-nav-dropdown').slideUp('fast');
-  
-        } else {
-  
-          $(this).find('.side-nav-dropdown').slideUp('fast');
-  
-        }
-  
-      });
-  
-      $('.side-nav .close-aside').on('click', function () {
-  
-        $('#' + $(this).data('close')).addClass('show-side-nav');
-  
-        contents.removeClass('margin');
-  
-      });
-  
-    }());
-  
-    // Start chart
   
     var chart = document.getElementById('myChart');
     Chart.defaults.global.animation.duration = 2000; // Animation duration
@@ -1530,6 +1455,4 @@ options: {
       }
     });
   
-    console.log(Chart.defaults.global);
   
-  });
